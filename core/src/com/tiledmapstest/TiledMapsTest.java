e2bd56098b0e897410518baaa874990630a08192
19Leandro87 <leandrololli87@gmail.com>
2020-08-17 22:09:15 +0200

    managed to import tiled map and set some basic camera controls, VERY IMPORTANT -> set Tile Layer Format NOT to be zstandard

--- core/src/com/tiledmapstest/TiledMapsTest.java
+++ core/src/com/tiledmapstest/TiledMapsTest.java
@@ -2,32 +2,112 @@ package com.tiledmapstest;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
-public class TiledMapsTest extends ApplicationAdapter {
-	SpriteBatch batch;
+public class TiledMapsTest extends ApplicationAdapter implements InputProcessor {
+
 	Texture img;
+	TiledMap tiledMap;
+	OrthographicCamera camera;
+	TiledMapRenderer renderer;
+	float width, height;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		width = Gdx.graphics.getWidth();
+		height = Gdx.graphics.getHeight();
+
+		tiledMap = new TmxMapLoader().load("androidTest.tmx");
+		renderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, width, height);
+		camera.update();
+
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		camera.update();
+		renderer.setView(camera);
+		renderer.render();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
 		img.dispose();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		if(keycode == Input.Keys.LEFT)
+			camera.translate(-32,0);
+		if(keycode == Input.Keys.RIGHT)
+			camera.translate(32,0);
+		if(keycode == Input.Keys.UP)
+			camera.translate(0,-32);
+		if(keycode == Input.Keys.DOWN)
+			camera.translate(0,32);
+		if(keycode == Input.Keys.NUM_1)
+			tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+		if(keycode == Input.Keys.NUM_2)
+			tiledMap.getLayers().get(1).setVisible(!tiledMap.getLayers().get(1).isVisible());
+		if(keycode == Input.Keys.A)
+			camera.zoom += 0.1f;
+		if(keycode == Input.Keys.Q)
+			camera.zoom -= 0.1f;
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		return false;
 	}
 }
