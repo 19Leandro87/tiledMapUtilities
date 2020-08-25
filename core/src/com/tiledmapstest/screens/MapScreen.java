3b1716c24bb340f985e6e8f07ee669b8ff84b3b3
19Leandro87 <leandrololli87@gmail.com>
2020-08-25 17:43:16 +0200

    got the camera centered on the map and tidied the code up

--- core/src/com/tiledmapstest/screens/MapScreen.java
+++ core/src/com/tiledmapstest/screens/MapScreen.java
@@ -27,7 +27,6 @@ public class MapScreen extends BaseScreen {
     OrthographicCamera camera;
     TiledMapRenderer renderer;
     MapProperties mapProperties;
-    float width, height, zoomFactor;
     int tiledMapWidth, tiledMapHeight, tiledLayerWidth, tiledLayerHeight;
     Vector3 mapCenter;
     Stage stage;
@@ -57,10 +56,8 @@ public class MapScreen extends BaseScreen {
         testActor.setSizeX(300f);
         testActor.setSizeY(300f);
 
-
         // below, the property "width" is the number of tiles, instead "tilewidth" is the width of the tiles in pixel
         // same goes for "height" and "tileheight"
-        //NOTE:
         tiledMapWidth = mapProperties.get("width", Integer.class) * mapProperties.get("tilewidth", Integer.class);
         tiledMapHeight = mapProperties.get("height", Integer.class) * mapProperties.get("tileheight", Integer.class);
 
@@ -68,37 +65,27 @@ public class MapScreen extends BaseScreen {
         tiledLayerWidth = ((TiledMapTileLayer) tiledMap.getLayers().get(0)).getWidth() * mapProperties.get("tilewidth", Integer.class);
         tiledLayerHeight = ((TiledMapTileLayer) tiledMap.getLayers().get(0)).getHeight() * mapProperties.get("tileheight", Integer.class);
 
-        //THE FOLLOWING SETS THE CAMERA POSITION TO THE CENTER OF THE MAP REGARDLESS OF THE ZOOM LEVEL
-        mapCenter = new Vector3(tiledLayerWidth/2f, tiledLayerHeight/2f, 0);
-        //camera.position.set(mapCenter);
-
-        width = Gdx.graphics.getWidth();
-        height = Gdx.graphics.getHeight();
-
         // -----------> GRAPHICAL ELEMENTS AND PROPERTIES <--- END
 
-        // -----------> CAMERA, VIEWPORT AND STAGE STUFF <--- BEGIN
+        // -----------> CAMERA, VIEWPORT AND STAGE <--- BEGIN
 
-        camera = new OrthographicCamera();
-        //zoomFactor = 3f;
-        camera.zoom = 1f;
-        camera.setToOrtho(false, tiledLayerWidth, tiledLayerHeight);
+        camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.zoom = 1.5f;
+        //camera.setToOrtho(false, tiledLayerWidth, tiledLayerHeight);
 
         viewport = new ScreenViewport(camera);
-        viewport.setScreenPosition(tiledLayerWidth/2, tiledLayerHeight/2);
-        viewport.update((int) width, (int) height, false);
-
-        camera.update();
 
         stage = new Stage(viewport);
 
+        //THE FOLLOWING SETS THE CAMERA POSITION IN THE CENTER OF THE "MAP LAYER"
+        mapCenter = new Vector3(tiledLayerWidth/2f, tiledLayerHeight/2f, 0);
+        camera.position.set(mapCenter);
+
         stage.addActor(testActor);
 
-        //System.out.println(camera.position.x + " " + width*2 + " " + height*2 + " " + tiledLayerWidth + " " + tiledLayerHeight);
+        // -----------> CAMERA, VIEWPORT AND STAGE <--- END
 
-        // -----------> CAMERA, VIEWPORT AND STAGE STUFF <--- END
-
-        // -----------> INPUT STUFF <--- BEGIN
+        // -----------> INPUT <--- BEGIN
 
         Gdx.input.setInputProcessor(stage);
         stage.addListener(new InputListener(){
@@ -131,12 +118,11 @@ public class MapScreen extends BaseScreen {
                     camera.position.set(mapCenter);
 
                 if(keycode == Input.Keys.Y)
-                    System.out.println(camera.position.x + " " + camera.position.y + " " + camera.zoom);
+                    System.out.println("x = " + camera.position.x + " y = " + camera.position.y + " zoom = " + camera.zoom);
 
                 return false;
             }
         });
-
 
     }
 
